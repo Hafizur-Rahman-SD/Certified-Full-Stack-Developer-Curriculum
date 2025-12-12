7bd01f2615b3845381de094166e6de5d1021b32f
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-12-12 18:07:02 +0600

    Create lab1.js

---
+++ JavaScript Certification/lab1.js
@@ -0,0 +1,43 @@
+
+// console.log("Hello! I'm your coding fun fact guide!");
+
+// let botName ="Hafizur";
+// let botLocation ="Dhaka";
+// let favoriteLanguage ="JavaScript";
+
+// console.log("My name is "+botName + " and I live on " +botLocation +".");
+
+// console.log("My favorite programming language is " +favoriteLanguage +".");
+
+
+// let codingFun = " Did you know?" +favoriteLanguage +" was created in just 10 days by Brendan Eich in 1995!";
+// console.log(codingFun);
+
+// codingFun += " It is now one of the most popular programming languages in the world, powering millions of websites and applications.";
+// console.log(codingFun);
+
+// console.log("Thanks for listening to my fun fact about " +favoriteLanguage + "! Happy coding!");
+
+// console.log("It was fun sharing these facts with you. Goodbye! -" + (botName) + "from" + (botLocation) +".");
+
+
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Hafiz";
+let botLocation = "Dhaka";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Fun fact about " + favoriteLanguage + ": it was created in 1995";
+console.log(codingFact);
+
+codingFact = "Another fun fact: " + favoriteLanguage + " can run on both server and browser";
+console.log(codingFact);
+
+codingFact = "Third Fact: " + favoriteLanguage + " is also known as JS";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
