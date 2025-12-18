0c24a66f49cd60e6bc4e3502febf7e6f37a73849
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-12-18 12:49:13 +0600

    Update testCode.js

--- JavaScript Certification/testCode.js
+++ JavaScript Certification/testCode.js
@@ -1,51 +1,62 @@
 
-// What Is Bracket Notation, and How Do You Access Characters from a String?
+// // What Is Bracket Notation, and How Do You Access Characters from a String?
 
 
-let abc="Hafizur";
-console.log(abc[5]);
+// // let abc="Hafizur";
+// // console.log(abc[5]);
 
 
-console.log(abc[abc.length -3]);  //its came last to first
+// // console.log(abc[abc.length -3]);  //its came last to first
 
 
-let firstLetter=abc[0] +abc[1] + abc[2];  //add string char 
-console.log(firstLetter);
+// // let firstLetter=abc[0] +abc[1] + abc[2];  //add string char 
+// // console.log(firstLetter);
 
 
-//new line in String \n
-let poem = "Roses are red. \nViolets are blue. \nSugar is sweet. \nAnd so are you.";
-console.log(poem);
+// // //new line in String \n
+// // let poem = "Roses are red. \nViolets are blue. \nSugar is sweet. \nAnd so are you.";
+// // console.log(poem);
 
-let poem2 = "Roses are red. \n 'I am your Dad' ";
-console.log(poem2);
+// // let poem2 = "Roses are red. \n 'I am your Dad' ";
+// // console.log(poem2);
 
-let day="To\day's day name Is Monday";
-console.log(day);
-
-
-const name="Hafizur";
-const age=22;
-const country="Bangladesh";
-const message= `my name is ${name}. I am ${age} years old. I live on ${country}.`;
-console.log(message);
+// // let day="To\day's day name Is Monday";
+// // console.log(day);
 
 
-const song="I love you \n You love me \n We are a happy family";
-const score=9.5;
-const highScore=10;
-const output= `one of my favourite song is : "${song}". \n I rated it ${(score / highScore) *100}%`;
-console.log(output);
+// // const name="Hafizur";
+// // const age=22;
+// // const country="Bangladesh";
+// // const message= `my name is ${name}. I am ${age} years old. I live on ${country}.`;
+// // console.log(message);
 
 
-//substring in a string\
-let sentence = "Javascript is awsome, and javascript is fun.";
-let position = sentence.indexOf("javaScript"); //case sensitive
-console.log(position); // Output: -1
-
-let position2 = sentence.toLowerCase().indexOf("javascript");
-console.log(position2); // Output: 0
+// // const song="I love you \n You love me \n We are a happy family";
+// // const score=9.5;
+// // const highScore=10;
+// // const output= `one of my favourite song is : "${song}". \n I rated it ${(score / highScore) *100}%`;
+// // console.log(output);
 
 
-//promt method 
+// // //substring in a string\
+// // let sentence = "Javascript is awsome, and javascript is fun.";
+// // let position = sentence.indexOf("javaScript"); //case sensitive
+// // console.log(position); // Output: -1
+
+// // let position2 = sentence.toLowerCase().indexOf("javascript");
+// // console.log(position2); // Output: 0
+
+
+// //promt method 
+
+// let a =0;
+// let b =0;
+// let result =a/b
+
+// if (Number.isNaN(result)){
+//   console.log("This operation does not compute");
+// }
+
+// console.log("The result is: " + result);
+
 
