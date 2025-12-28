7b277bdc986436194d791d102682a8006868d0ed
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-12-28 23:38:46 +0600

    d

---
+++ python/DataType& Variable/String/inAndlen.py
@@ -0,0 +1,9 @@
+
+my_name="Hafizur Rahman"
+
+print ("Hafi" in my_name)
+print("Rahman" not in my_name)
+print("Rony" in my_name)
+
+print("Length of my_name:", len(my_name))
+print("Type of my_name:", type(my_name))
---
+++ python/DataType& Variable/variable.py
@@ -0,0 +1,12 @@
+
+name='Hafizur Rahman'
+age=23
+price=2500.50
+is_student=True
+fruits=['apple','banana','orange']
+
+print("Name:", name, "Type:", type(name))
+print("Age:", age, "Type:", type(age))
+print("Price:", price, "Type:", type(price))
+print("Is Student:", is_student, "Type:", type(is_student))
+print("Fruits:", fruits, "Type:", type(fruits))
